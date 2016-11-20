7e8024681a6dc2d309ebbb20f0ae0843aa4b12df
KenSentMe <jeroen@vandenieuwenhof.nl>
2016-11-20 17:11:52 +0100

    Started using different files. First step is creating a seperate file for the game objects.

--- house.rb
+++ house.rb
@@ -4,6 +4,8 @@
 # => Go to other room in certain direction
 # => Look at objects in Room
 
+require "./house_gameobjects.rb"
+
 module Commands
 
   # The look method is for objects in a room or in your inventory
@@ -53,7 +55,7 @@ module Commands
     end
   end
 
-  # The use command is for combining objects. 
+  # The use command is for combining objects.
   def use(command)
     if command == "use"
       puts "Use what?"
@@ -119,18 +121,6 @@ class Room
   end
 end
 
-# Create the object class for all objects in the game
-class GameObject
-  attr_reader :name, :description, :pickup
-  attr_accessor :action
-  def initialize(name,description,action,pickup,visible)
-    @name = name
-    @description = description
-    @action = action
-    @pickup = pickup
-  end
-end
-
 # This is the prompt where players type their commands. It displays the current room name and sends commands to the evaluate method
 def prompt
   include Commands
@@ -158,18 +148,6 @@ def evaluate(command)
     puts "I don't understand #{command}"
   end
   prompt
-end
-
-def create_objects
-  $fork = GameObject.new("Fork", "This is a fork", {}, true, true)
-  $bag = GameObject.new("Bag", "This is a bag", {}, false, true)
-  $balloon = GameObject.new("Balloon", "This is an inflated balloon", {}, true, true)
-  $punched_balloon = GameObject.new("Punched balloon", "It's a deflated balloon with a tiny hole in it", {}, false, false)
-  create_object_actions
-end
-
-def create_object_actions
-  $fork.action = {verb: "combine", combine: "balloon", text: "Using the fork on the balloon, does the obvious thing. You get a deflated balloon with a hole in it.", result: {key: :punched_balloon, object: $punched_balloon, text: "Punched balloon"}}
 end
 
 # The rooms have to be created first with empty hashes for the directions
