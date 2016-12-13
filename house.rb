2207880ac2c5b59b605806f9266f074eab556952
KenSentMe <jeroen@vandenieuwenhof.nl>
2016-12-13 13:40:33 +0100

    Added bot functionality

--- house.rb
+++ house.rb
@@ -1,29 +1,11 @@
-# Outline
-# Room Class
-# => Look around in Room
-# => Go to other room in certain direction
-# => Look at objects in Room
-
+require 'telegram_bot'
 require "./house_game_objects.rb"
 require "./house_commands.rb"
 require "./house_rooms.rb"
 
-# This is the prompt where players type their commands. It displays the current room name and sends commands to the evaluate method
-def prompt
-  include Commands
-  print "[#{$current_room.name}]:>"
-  command = gets.chomp.downcase
-  # split the command so we have seperate word and params
-  command = command.split(' ')
-  # grab the first word and define it as the word of the command
-  command_word = command.shift
-  # and the rest of the command is one or more parameters
-  command_params = command
-  evaluate(command_word, command_params)
-end
-
 # Here the commands are evaluated and (if necessary) sent to the appropriate method
 def evaluate(command_word, command_params)
+  include Commands
   case command_word
   when "q", "quit"
     puts "Bye, bye!"
@@ -39,9 +21,8 @@ def evaluate(command_word, command_params)
   when "use"
     use($current_room, command_params)
   else
-    puts "I don't understand #{command_params}"
+    $reply_text = "I don't understand #{command_word}"
   end
-  prompt
 end
 
 # Calling the methods to create the rooms and objects and set the directions
@@ -57,4 +38,17 @@ $current_room = $room1
 $inventory = {}
 
 # Start the game by running the prompt
-prompt
+bot = TelegramBot.new(token: '319896844:AAEa9Ojtb7NppgeUDXP3QwRK7TujOwodmKE')
+bot.get_updates(fail_silently: true) do |message|
+  puts "@#{message.from.username}: #{message.text}"
+  command = message.get_command_for(bot)
+  command = command.downcase
+  command = command.split(' ')
+  command_word = command.shift
+  command_params = command
+  evaluate(command_word, command_params)
+  message.reply do |reply|
+    reply.text = $reply_text
+    reply.send_with(bot)
+  end
+end
--- house_commands.rb
+++ house_commands.rb
@@ -14,39 +14,39 @@ module Commands
       current_room.game_objects.each do |k,o|
         room_objects << o.room_text
       end
-      puts "#{current_room.description}. #{room_objects.join(' ')} From here you can go #{converted_directions.join(', ')}."
+      $reply_text = "#{current_room.description}. #{room_objects.join(' ')} From here you can go #{converted_directions.join(', ')}."
     else
       # look at an object in the room (if it exists)
       object = command_params.join('_').intern
       if current_room.game_objects.key?(object)
         object_instance = current_room.game_objects[object]
-        puts object_instance.description
+        $reply_text =object_instance.description
       elsif $inventory.key?(object)
         object_instance = $inventory[object]
-        puts object_instance.description
+        $reply_text = object_instance.description
       else
-        puts "That object doesn't exist, at least not in this room."
+        $reply_text = "That object doesn't exist, at least not in this room."
       end
     end
   end
 
   def get(current_room, command_params)
     if command_params.empty?
-      puts "I don't get it, what do you want?"
+      $reply_text = "I don't get it, what do you want?"
     elsif command_params.length > 1
-      puts "Wow, easy there. Try picking up one item at a time."
+      $reply_text = "Wow, easy there. Try picking up one item at a time."
     else
       object = command_params[0].intern
       if current_room.game_objects.key?(object)
         object_instance = current_room.game_objects[object]
         if object_instance.pickup
           $inventory[object] = current_room.game_objects.delete(object)
-          puts "You add #{object_instance.name} to your inventory."
+          $reply_text = "You add #{object_instance.name} to your inventory."
         else
-          puts "You can't pick that up."
+          $reply_text = "You can't pick that up."
         end
       else
-        puts "You can't get that, at least not now."
+        $reply_text = "You can't get that, at least not now."
       end
     end
   end
@@ -54,11 +54,11 @@ module Commands
   # The use command is for combining objects.
   def use(current_room, command_params)
     if command_params.empty?
-      puts "Use what?"
+      $reply_text = "Use what?"
     elsif command_params.length > 3
-      puts "What are you trying to do, make a coctail? Combining two items should be sufficient."
+      $reply_text = "What are you trying to do, make a coctail? Combining two items should be sufficient."
     elsif command_params[1] != "on"
-      puts "That's not the right way to combine stuff"
+      $reply_text = "That's not the right way to combine stuff"
     else
       command_params.delete("on")
       object1 = command_params[0].intern
@@ -70,10 +70,10 @@ module Commands
         object1_instance = $inventory[object1]
         # check if it can be combined with the second object
         if object1_instance.action[:use_with] != object2.to_s
-          puts "You can't do that, at least not now."
+          $reply_text = "You can't do that, at least not now."
         else
           # print out the result of the combination
-          puts object1_instance.action[:text]
+          $reply_text = object1_instance.action[:text]
           new_object_key = object1_instance.action[:result]
           object1_instance.action[:deleted_objects].each do |d|
             $inventory.delete(d)
@@ -88,43 +88,43 @@ module Commands
         object1_instance = $inventory[object1]
         # check if the first object can be combined with the second object
         if object1_instance.action[:combine_with] != object2.to_s
-          puts "You can't do that, at least not now."
+          $reply_text = "You can't do that, at least not now."
         else
           # print out the result of the combination
-          puts object1_instance.action[:text]
+          $reply_text = object1_instance.action[:text]
         end
       else
-        puts "That doesn't make sense"
+        $reply_text = "That doesn't make sense"
       end
     end
   end
 
   def inventory
     if $inventory.empty?
-      puts "Your inventory is empty."
+      $reply_text = "Your inventory is empty."
     else
       inventory_list = []
       $inventory.each do |s,i|
         inventory_list << i.name
       end
-      puts "In your inventory you have the following items: #{inventory_list.join(', ')}"
+      $reply_text = "In your inventory you have the following items: #{inventory_list.join(', ')}"
     end
   end
 
   def go(current_room, command_params)
     if command_params.empty?
-      puts "Please specify a direction"
+      $reply_text = "Please specify a direction"
     elsif command_params.length == 1 || command_params[1].length == 1
       direction = command_params[0].intern
       # Check if the entered direction is an actual direction
       if $current_room.directions.key?(direction)
         $current_room = current_room.directions[direction]
       else
-        puts "You can't go that way"
+        $reply_text = "You can't go that way"
       end
     else
       wrong_direction = command_params.join(" ")
-      puts "I don't understand the direction #{wrong_direction}"
+      $reply_text = "I don't understand the direction #{wrong_direction}"
     end
   end
 end
