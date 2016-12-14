5ec2bfdae9f8898829f55e6a4cd08189bb803edf
KenSentMe <jeroen@vandenieuwenhof.nl>
2016-12-14 17:27:11 +0100

    Bugfix for room name

--- house_commands.rb
+++ house_commands.rb
@@ -121,7 +121,8 @@ module Commands
         $current_room = current_room.directions[direction]
         direction_conversions = {n: "North", s: "South", e: "East", w: "West"}
         full_direction = direction_conversions[direction]
-        $reply_text = "You go #{full_direction} and enter #{current_room.name}"
+        room_name = $current_room.name
+        $reply_text = "You go #{full_direction} and enter #{room_name}"
       else
         $reply_text = "You can't go that way"
       end
