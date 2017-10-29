4d5979770179b2b66aff11fb0cc3e9e1100ee2b2
KenSentMe <jeroen@vandenieuwenhof.nl>
2017-10-29 15:15:30 +0000

    Added the option to switch to command line mode by giving the 'nobot' argument at start

--- house.rb
+++ house.rb
@@ -37,18 +37,52 @@ $current_room = $room1
 # Creating an empty inventory array
 $inventory = {}
 
-# Start the game by running the prompt
-bot = TelegramBot.new(token: '319896844:AAEa9Ojtb7NppgeUDXP3QwRK7TujOwodmKE')
-bot.get_updates(fail_silently: true) do |message|
-  puts "@#{message.from.username}: #{message.text}"
-  command = message.get_command_for(bot)
-  command = command.downcase
-  command = command.split(' ')
-  command_word = command.shift
-  command_params = command
-  evaluate(command_word, command_params)
-  message.reply do |reply|
-    reply.text = $reply_text
-    reply.send_with(bot)
+# This game can operate in 2 modes. The default mode is the bot mode, where
+# the game runs in a chatbot (in this case Telegram). When the argument nobot
+# is given, the game runs in command line.
+
+# Setting up the function to start the bot
+def start_bot
+  bot = TelegramBot.new(token: '319896844:AAEa9Ojtb7NppgeUDXP3QwRK7TujOwodmKE')
+  bot.get_updates(fail_silently: true) do |message|
+    puts "@#{message.from.username}: #{message.text}"
+    command = message.get_command_for(bot)
+    command = command.downcase
+    command = command.split(' ')
+    command_word = command.shift
+    command_params = command
+    evaluate(command_word, command_params)
+    message.reply do |reply|
+      reply.text = $reply_text
+      reply.send_with(bot)
+    end
   end
+end
+
+# Setting up the function to run the game in the prompt
+def start_prompt
+  loop do
+    print "[#{$current_room.name}]:>"
+    command = STDIN.gets.chomp.downcase
+    # split the command so we have seperate word and params
+    command = command.split(' ')
+    # grab the first word and define it as the word of the command
+    command_word = command.shift
+    # and the rest of the command is one or more parameters
+    command_params = command
+    evaluate(command_word, command_params)
+    puts $reply_text
+  end
+end
+
+# Check the mode in which the game runs by grabbing the argument from the
+# command line
+game_mode = ARGV
+case game_mode[0]
+when nil
+  start_bot
+when "nobot"
+  start_prompt
+else
+  puts "Unknow argument #{game_mode}"
 end
